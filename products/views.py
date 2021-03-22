3291c2ec9bbd10ab4be62a53fb59c380297134bc
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-22 06:09:01 +0200

    Add product_list_view

--- products/views.py
+++ products/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 
 
 class ProductViewSet(viewsets.ViewSet):
+
     def product_list_view(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
     def product_create_view(self, request):
         pass
