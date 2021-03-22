565946119eec8788bd79a179f0213cd2ed5616a4
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-22 06:00:08 +0200

    Add ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+
+
+class ProductViewSet(viewsets.ViewSet):
+    def product_list_view(self, request):
+        pass
+
+    def product_create_view(self, request):
+        pass
+
+    def product_retrieve_view(self, request, pk=None):
+        pass
+
+    def product_update_view(self, pk=None):
+        pass
+
+    def product_delete_view(self, request, pk=None):
+        pass
