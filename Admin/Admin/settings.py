7af9c5d10662043ba3dc3dea094b4cea625d43c0
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-05-05 05:51:16 +0300

    Add environ

--- Admin/Admin/settings.py
+++ Admin/Admin/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '((ff)qoq=!!w*n2vd+hy%xp$8a2#lxgaq)jhmy7y@i(m_)m@qj'
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
